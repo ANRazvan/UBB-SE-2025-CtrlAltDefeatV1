092d9a398763a5626dbe4da3422f8ee45fb1252f
car-men01 <carmen.beudean@stud.ubbcluj.ro>
2025-03-21 19:05:24 +0200

    Friends List View and View Model

---
+++ SocialStuff/SocialStuff/View/FriendsListView.xaml.cs
@@ -0,0 +1,130 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using SocialStuff.Services;
+using SocialStuff.ViewModel;
+using SocialStuff.Data;
+using SocialStuff.Model;
+using System.Collections.Specialized;
+
+namespace SocialStuff.View
+{
+    public sealed partial class FriendsListView : Window
+    {
+        public FriendsListViewModel ViewModel { get; private set; }
+
+        public FriendsListView()
+        {
+            this.InitializeComponent();
+
+            // Initialize the ViewModel with dependencies
+            Repository repo = new Repository();
+            UserService userService = new UserService(repo);
+            ViewModel = new FriendsListViewModel(userService);
+
+            // Load initial data
+            ViewModel.LoadFriends();
+
+            // Subscribe to the CollectionChanged event
+            ViewModel.Friends.CollectionChanged += Friends_CollectionChanged;
+
+            // Initial check
+            UpdateNoFriendsTextVisibility();
+        }
+
+        private void AddFriendButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open the AddFriend window
+            //AddFriend addFriendView = new AddFriend();
+            //addFriendView.Activate();
+        }
+
+        private void RemoveFriendButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.CommandParameter is User user)
+            {
+                ViewModel.SelectedFriend = user;
+                if (ViewModel.RemoveFriendCommand.CanExecute(null))
+                {
+                    ViewModel.RemoveFriendCommand.Execute(null);
+                    UpdateNoFriendsTextVisibility(); // Update visibility after removing a friend
+                }
+            }
+        }
+
+        private void Friends_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            UpdateNoFriendsTextVisibility();
+        }
+
+        private void UpdateNoFriendsTextVisibility()
+        {
+            NoFriendsTextBlock.Visibility = ViewModel.Friends.Count == 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
+
+
+
+//using Microsoft.UI.Xaml;
+//using Microsoft.UI.Xaml.Controls;
+//using SocialStuff.Services;
+//using SocialStuff.ViewModel;
+//using SocialStuff.Data;
+//using SocialStuff.Model;
+//using System.Collections.Specialized;
+
+//namespace SocialStuff.View
+//{
+//    public sealed partial class FriendsListView : Window
+//    {
+//        public FriendsListViewModel ViewModel { get; private set; }
+
+//        public FriendsListView()
+//        {
+//            this.InitializeComponent();
+
+//            // Initialize the ViewModel with dependencies
+//            Repository repo = new Repository();
+//            UserService userService = new UserService(repo);
+//            ViewModel = new FriendsListViewModel(userService);
+
+//            // Load initial data
+//            ViewModel.LoadFriends();
+
+//            // Subscribe to the CollectionChanged event
+//            ViewModel.Friends.CollectionChanged += Friends_CollectionChanged;
+
+//            // Initial check
+//            UpdateNoFriendsTextVisibility();
+//        }
+
+//        private void AddFriendButton_Click(object sender, RoutedEventArgs e)
+//        {
+//            // Open the AddFriend window
+//            //AddFriend addFriendWindow = new AddFriend();
+//            //addFriendWindow.Activate();
+//        }
+
+//        private void RemoveFriendButtonClick(object sender, RoutedEventArgs e)
+//        {
+//            if (sender is Button button && button.CommandParameter is User user)
+//            {
+//                ViewModel.SelectedFriend = user;
+//                if (ViewModel.RemoveFriendCommand.CanExecute(null))
+//                {
+//                    ViewModel.RemoveFriendCommand.Execute(null);
+//                }
+//            }
+//        }
+
+//        private void Friends_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+//        {
+//            UpdateNoFriendsTextVisibility();
+//        }
+
+//        private void UpdateNoFriendsTextVisibility()
+//        {
+//            NoFriendsTextBlock.Visibility = ViewModel.Friends.Count == 0 ? Visibility.Visible : Visibility.Collapsed;
+//        }
+//    }
+//}
--- SocialStuff/SocialStuff/ViewModel/FriendsListViewModel.cs
+++ SocialStuff/SocialStuff/ViewModel/FriendsListViewModel.cs
@@ -5,10 +5,8 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Windows;
 using System.Windows.Input;
 using SocialStuff.Model;
-using Windows.System.UserProfile;
 
 namespace SocialStuff.ViewModel
 {
@@ -25,48 +23,59 @@ namespace SocialStuff.ViewModel
             friends = new ObservableCollection<User>(userService.GetRepo().GetUserFriendsList(userService.GetCurrentUser()));
             searchQuery = "";
         }
+
         public ObservableCollection<User> Friends
         {
             get { return friends; }
             set
             {
-                friends = value;
-                OnPropertyChanged();
+                if (friends != value)
+                {
+                    friends = value;
+                    OnPropertyChanged();
+                }
             }
         }
+
         public User SelectedFriend
         {
             get { return selectedFriend; }
             set
             {
-                selectedFriend = value;
-                OnPropertyChanged();
+                if (selectedFriend != value)
+                {
+                    selectedFriend = value;
+                    OnPropertyChanged();
+                }
             }
         }
+
+        public void LoadFriends()
+        {
+            Friends = new ObservableCollection<User>(userService.GetRepo().GetUserFriendsList(userService.GetCurrentUser()));
+        }
+
         public string SearchQuery
         {
             get { return searchQuery; }
             set
             {
-                searchQuery = value;
-                OnPropertyChanged();
-                Friends = new ObservableCollection<User>(userService.GetRepo().GetUserFriendsList(userService.GetCurrentUser()).Where(f => f.GetUsername().Contains(searchQuery)));
+                if (searchQuery != value)
+                {
+                    searchQuery = value;
+                    OnPropertyChanged();
+                    UpdateFilteredFriends();
+                }
             }
         }
-        //public ICommand AddFriendCommand
-        //{
-        //    get
-        //    {
-        //        return new RelayCommand(param =>
-        //        {
-        //            if (SelectedFriend != null)
-        //            {
-        //                userService.AddFriend(userService.GetCurrentUser(), SelectedFriend.ID);
-        //                Friends = new ObservableCollection<User>(userService.GetRepo().GetFriends(userService.GetCurrentUser()));
-        //            }
-        //        });
-        //    }
-        //}
+
+        private void UpdateFilteredFriends()
+        {
+            var filteredFriendIds = userService.FilterFriends(searchQuery, userService.GetCurrentUser());
+            var filteredFriends = userService.GetRepo().GetUsersList().Where(user => filteredFriendIds.Contains(user.GetUserId()));
+            Friends = new ObservableCollection<User>(filteredFriends);
+        }
+
         public ICommand RemoveFriendCommand
         {
             get
@@ -76,16 +85,120 @@ namespace SocialStuff.ViewModel
                     if (SelectedFriend != null)
                     {
                         userService.RemoveFriend(userService.GetCurrentUser(), SelectedFriend.GetUserId());
-                        Friends = new ObservableCollection<User>(userService.GetRepo().GetUserFriendsList(userService.GetCurrentUser()));
+                        Friends.Remove(SelectedFriend); // Remove the friend from the collection
                     }
                 });
             }
         }
+
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
-    
 }
+
+
+
+
+//using SocialStuff.Services;
+//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.ComponentModel;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Windows.Input;
+//using SocialStuff.Model;
+
+//namespace SocialStuff.ViewModel
+//{
+//    public class FriendsListViewModel : INotifyPropertyChanged
+//    {
+//        private UserService userService;
+//        private ObservableCollection<User> friends;
+//        private User selectedFriend;
+//        private string searchQuery;
+
+//        public FriendsListViewModel(UserService userService)
+//        {
+//            this.userService = userService;
+//            friends = new ObservableCollection<User>(userService.GetRepo().GetUserFriendsList(userService.GetCurrentUser()));
+//            searchQuery = "";
+//        }
+
+//        public ObservableCollection<User> Friends
+//        {
+//            get { return friends; }
+//            set
+//            {
+//                if (friends != value)
+//                {
+//                    friends = value;
+//                    OnPropertyChanged();
+//                }
+//            }
+//        }
+
+//        public User SelectedFriend
+//        {
+//            get { return selectedFriend; }
+//            set
+//            {
+//                if (selectedFriend != value)
+//                {
+//                    selectedFriend = value;
+//                    OnPropertyChanged();
+//                }
+//            }
+//        }
+
+//        public void LoadFriends()
+//        {
+//            Friends = new ObservableCollection<User>(userService.GetRepo().GetUserFriendsList(userService.GetCurrentUser()));
+//        }
+
+//        public string SearchQuery
+//        {
+//            get { return searchQuery; }
+//            set
+//            {
+//                if (searchQuery != value)
+//                {
+//                    searchQuery = value;
+//                    OnPropertyChanged();
+//                    UpdateFilteredFriends();
+//                }
+//            }
+//        }
+
+//        private void UpdateFilteredFriends()
+//        {
+//            var filteredFriendIds = userService.FilterFriends(searchQuery, userService.GetCurrentUser());
+//            var filteredFriends = userService.GetRepo().GetUsersList().Where(user => filteredFriendIds.Contains(user.GetUserId()));
+//            Friends = new ObservableCollection<User>(filteredFriends);
+//        }
+
+//        public ICommand RemoveFriendCommand
+//        {
+//            get
+//            {
+//                return new RelayCommand(param =>
+//                {
+//                    if (SelectedFriend != null)
+//                    {
+//                        userService.RemoveFriend(userService.GetCurrentUser(), SelectedFriend.GetUserId());
+//                        Friends = new ObservableCollection<User>(userService.GetRepo().GetUserFriendsList(userService.GetCurrentUser()));
+//                    }
+//                });
+//            }
+//        }
+
+//        public event PropertyChangedEventHandler PropertyChanged;
+//        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+//        {
+//            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+//        }
+//    }
+//}
